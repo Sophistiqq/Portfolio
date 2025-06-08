cee7fe476c92bc21f2b675b75880bcacea5b6545
Roi Borromeo <roi.for.school@gmail.com>
2025-06-08 09:09:21 +0000

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Router from "svelte-spa-router";
+  import Home from "./routes/Home.svelte";
+  const routes = {
+    "/": Home,
+  };
+</script>
+
+<Router {routes} />
---
+++ src/lib/Nav.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { Home } from "lucide-svelte"
+</script>
+
+<header>
+
+</header>
